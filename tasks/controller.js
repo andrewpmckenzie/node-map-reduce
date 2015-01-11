541f391899d010d52137cb8b10247e4e2a52bf0b
Andrew McKenzie <andrewpmckenzie@gmail.com>
2015-01-11 13:30:34 -0500

    Controller: add grunt task to start server

---
+++ tasks/controller.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+  grunt.config.merge({
+
+    // https://github.com/ericclemmons/grunt-express-server
+    express: {
+      controllerServer: {
+        options: {
+          script: 'app/controller/bin/controller',
+          node_env: grunt.option('env') || 'development',
+          port: grunt.option('port') || 0
+        }
+      }
+    },
+
+    // https://github.com/gruntjs/grunt-contrib-watch
+    watch: {
+      options: {
+        nospawn: true
+      },
+
+      controllerServerDev: {
+        files: [ 'app/controller/**/*' ],
+        tasks: [ 'express:controllerServer' ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-express-server');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.task.registerTask('controller:start', ['express:controllerServer', 'watch:controllerServerDev']);
+};
