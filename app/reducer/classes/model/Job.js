a9702df72c976ed54e95d98ad9ae6b2d7c12d745
Andrew McKenzie <andrewpmckenzie@gmail.com>
2015-01-22 00:51:51 -0500

    Reducer: perform the reduction (well, really the folding)
    This is slow, and we need to be smarter about the types
    returned by and passed to the reducer.

--- app/reducer/classes/model/Job.js
+++ app/reducer/classes/model/Job.js
@@ -14,9 +14,12 @@ var Job = function(
   this.id_ = id  || (function() { throw new Error('id not provided'); })();
   this.reduceFunction_ = reduceFunction || (function() { throw new Error('reduceFunction not provided'); })();
   this.client_ = client;
+
+  // TODO: find an alternative to Sandbox. It is sssslllllooooooowwwwwwwwww.
   this.sandbox_ = new Sandbox({
     timeout: 1000 // TODO: make this configurable
   });
+
   this.results_ = {};
 };
 
@@ -34,7 +37,41 @@ Job.prototype = {
 
   process: function(chunkId, key, values, partitionerClient) {
     log('process(%s, %s, %o) called', chunkId, key, values);
-    partitionerClient.reduced(this.id_, chunkId, key);
+
+    var memo = this.results_[key];
+
+    // TODO: how should we handle types?
+    var escapedMemo = memo ? jsesc(memo) : '';
+    var escapedValues = jsesc(values);
+
+    // TODO: pass through appropriate types
+    var wrappedFunction = 'JSON.stringify((' + this.reduceFunction_ + ')(\'' + escapedMemo + '\', \'' + escapedValues + '\'))';
+
+    this.sandbox_.run(wrappedFunction, function(output) {
+      var consoleOutput = output.console;
+      var rawResult = output.result;
+      var errorMessage = null;
+      var didError = false;
+
+      try {
+        var result = JSON.parse(rawResult.replace(/^'/, '').replace(/'$/, ''));
+      } catch(e) {
+        errorMessage = rawResult;
+        didError = true;
+        log('ERROR Bad kv caught: [k] %s, [v] %o, [memo] %o, [chunkId] %s', key, values, memo, chunkId);
+        log('ERROR %s', wrappedFunction);
+        log('ERROR throws %s', errorMessage);
+      }
+
+      log('Processed chunk [%s][%s]: %o', chunkId, key, result);
+      (consoleOutput || []).forEach(function(consoleMessage) { log('  > %s', consoleMessage); });
+      log('Memory state: [%s/%s]', process.memoryUsage().heapUsed, process.memoryUsage().heapTotal);
+
+      var error = didError ? errorMessage || 'unknown error' : undefined;
+      partitionerClient.reduced(this.id_, chunkId, key, error);
+
+      this.results_[key] = result;
+    }.bind(this));
   },
 
   results: function() {
