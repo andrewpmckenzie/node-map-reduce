4d4fb1cea5f1a1f0490262a3462a9c39043df57d
Andrew McKenzie <andrewpmckenzie@gmail.com>
2015-01-22 00:44:26 -0500

    Partitioner: add throttled queue to ReducerClient
    Also:
     - emit an event when a chunk is reduced
    
    This is breaking the style everywhere else in the
    app where socket decoration is left to the App file
    to maximise discovery. This way is growing on me
    as the nicer option though - will probably refactor
    elsewhere later.

--- app/partitioner/classes/client/ReducerClient.js
+++ app/partitioner/classes/client/ReducerClient.js
@@ -1,12 +1,45 @@
 var util = require('util');
+
 var Client = require('../../../common/base/Client');
+
+var MAX_PROCESSING = 5;
 
 var ReducerClient = Client.extend({
   logName: 'nmr:partitioner:ReducerClient',
 
+  constructor: function() {
+    ReducerClient.super_.apply(this, arguments);
+
+    this.queue_ = [];
+    this.numberProcessing_ = 0;
+
+    this.socket().on('job:kv:reduced', this.handleReducedChunkKey_.bind(this));
+  },
+
   reduce: function(jobId, chunkId, key, values) {
-    this.send('job:kv:process', {jobId: jobId, chunkId: chunkId, key: key, values: values});
+    this.queue_.push({jobId: jobId, chunkId: chunkId, key: key, values: values});
+    this.maybeSendNext_();
+  },
+
+  maybeSendNext_: function() {
+    if (this.numberProcessing_ < MAX_PROCESSING && this.queue_.length > 0) {
+      this.send('job:kv:process', this.queue_.pop());
+      this.numberProcessing_++;
+    }
+  },
+
+  handleReducedChunkKey_: function(options) {
+    this.log('handleReducedChunkKey_(%o) called.', options);
+    var chunkId = options.chunkId;
+    var key = options.key;
+    var errors = options.error;
+
+    this.emit('reduced', {chunkId: chunkId, key: key, error: errors});
+
+    this.numberProcessing_--;
+    this.maybeSendNext_();
   }
+
 });
 
 module.exports = ReducerClient;
