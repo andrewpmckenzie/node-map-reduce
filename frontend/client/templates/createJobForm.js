2d281022f1aa946ea776265bc616aa251f189c19
Andrew McKenzie <andrewpmckenzie@gmail.com>
2015-02-28 23:05:24 -0500

    Frontend: add createJobForm with codemirror for code fields

--- frontend/client/templates/createJobForm.js
+++ frontend/client/templates/createJobForm.js
@@ -1,3 +1,13 @@
 Template.createJobForm.events({
   'click .cancel': function() { Session.set('actionSectionTemplate', 'actionSectionButtons'); return false; }
 });
+
+Template.createJobForm.rendered = function() {
+  this.findAll('.editor').forEach(function(el) {
+    // http://codemirror.net
+    CodeMirror.fromTextArea(el, {
+      lineNumbers: true,
+      mode: 'javascript'
+    });
+  });
+};
