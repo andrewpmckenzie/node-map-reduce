2abaf8c0ab104d75ceec336c138b8f199b68df2c
Andrew McKenzie <andrewpmckenzie@gmail.com>
2015-03-01 22:10:54 -0500

    Frontend: extract job detail assembly to collection util

--- frontend/client/templates/jobList.js
+++ frontend/client/templates/jobList.js
@@ -1,16 +1,16 @@
 Template.jobList.helpers({
   jobs: function () {
-    var jobMap = {};
-    Jobs.find().forEach(function(job) {
-      var id = job.jobId;
-      if (! (id in jobMap)) {
-        jobMap[id] = {id: id};
-      }
-      jobMap[id][job.serverType] = jobMap[id][job.serverType] || {};
-      jobMap[id][job.serverType][job.address] = job.details;
-    });
-
+    var jobMap = JobsHelper.getAll();
     this.data = Object.keys(jobMap).map(function(key) { return jobMap[key]; });
     return this.data;
   }
+});
+
+Template.jobList_item.helpers({
+  // Flatten the controller, as there will only ever be one
+  controller: function() { return this.controller['-']; }
+});
+
+Template.jobList_item.events({
+  'click': function(event, template) { window.location.href = '/job/' + this.id; }
 });
--- frontend/common/collections/Jobs.js
+++ frontend/common/collections/Jobs.js
@@ -1,5 +1,31 @@
 Jobs = new Mongo.Collection("jobs");
 
+JobsHelper = {
+  getAll: function() {
+    return JobsHelper.unify_(Jobs.find());
+  },
+
+  getOne: function(jobId) {
+    var map = JobsHelper.unify_(Jobs.find({jobId: jobId}));
+    return map[jobId];
+  },
+
+  unify_: function(jobs) {
+    var jobMap = {};
+
+    jobs.forEach(function(job) {
+      var id = job.jobId;
+      if (! (id in jobMap)) {
+        jobMap[id] = {id: id};
+      }
+      jobMap[id][job.serverType] = jobMap[id][job.serverType] || {};
+      jobMap[id][job.serverType][job.address] = job.details;
+    });
+
+    return jobMap;
+  }
+};
+
 if (Meteor.isServer) {
   // Start with a fresh DB
   Jobs.remove({});
